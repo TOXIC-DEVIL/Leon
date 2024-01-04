41b28e663ce4632e9c6c87fe42eae63036442f27
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-01-04 07:32:22 +0530

    Update getenv.js

--- commands/getenv.js
+++ commands/getenv.js
@@ -5,10 +5,10 @@ module.exports = {
   info: 'Gets the value of environment variable of given key.',
   func: async (sock, msg, text) => {
     if ((process.env?.PLATFORM).toLowerCase() !== 'heroku') return await msg.reply({ text: '*You must deploy bot on heroku for bot administrative commands.*' });
-    if (!text) return await msg.reply({ text: '*Please enter the environment variable key to delete!*' });
+    if (!text) return await msg.reply({ text: '*Please enter the environment variable key!*' });
     let value = await Heroku('getenv', text);
     if (!value) {
-      return await msg.reply({ text: '*There is no such environment variable key to delete!*' });
+      return await msg.reply({ text: '*There is no such environment variable key!*' });
     } else {
       return await msg.reply({ text: '*' + text + ': ' + value + '*' });
     }
