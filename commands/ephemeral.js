854215acf35ecedae56f1a360a6c2e9d0842c22f
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 20:01:07 +0530

    Mode switcher

--- commands/ephemeral.js
+++ commands/ephemeral.js
@@ -3,6 +3,7 @@ const { WA_DEFAULT_EPHEMERAL } = require('@whiskeysockets/baileys');
 module.exports = {
   command: 'ephemeral',
   info: 'Manage disappearing message in this chat.',
+  private: false,
   func: async (sock, msg, text) => {
     if (msg.isGroup && (!(await msg.isAdmin(msg.me)))) return await msg.reply({ text: '*I am not an admin of this group!*' });
     let duration = text !== '' ? (text.includes('7') ? WA_DEFAULT_EPHEMERAL : text.includes('24') ? 86400 : text.includes('90') ? 7776000 : text.match(/off|disable/) ? false : WA_DEFAULT_EPHEMERAL) : WA_DEFAULT_EPHEMERAL;
