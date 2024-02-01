48caa39ae428117145214988bdbfa95c6246f307
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 20:09:28 +0530

    Mode switcher

--- commands/poll.js
+++ commands/poll.js
@@ -1,8 +1,9 @@
 module.exports = {
   command: 'poll',
   info: 'Creates a poll from given title and options.',
+  private: false,
   func: async (sock, msg, text) => {
     if (!text) return await msg.reply({ text: '*Please enter the title and options by splitting with | symbol!*\n\n*Example:*\n*- ' + (process.env?.PREFIX || '/') + 'poll title|option1|option2|option3' });
-    await msg.reply({ poll: { title: text.split('|')[0], options: text.split('|').slice(1) } });
+    return await msg.reply({ poll: { title: text.split('|')[0], options: text.split('|').slice(1) } });
   }
 };
