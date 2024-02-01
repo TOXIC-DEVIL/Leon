bc6d2c0a94106a62da8ea8f0e8744d6de7c2d711
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 20:08:57 +0530

    Mode switcher

--- commands/mute.js
+++ commands/mute.js
@@ -1,6 +1,7 @@
 module.exports = {
   command: 'mute',
   info: 'Only admins can send messages mode.',
+  private: false,
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in group!*' });
     if (!(await msg.isAdmin(msg.me))) return await msg.reply({ text: '*I am not an admin of this group!*' });
