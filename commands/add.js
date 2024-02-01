f97cfcbab08e1485d8b62e033b87dd641c3e205c
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 19:51:47 +0530

    Mode switcher

--- commands/add.js
+++ commands/add.js
@@ -1,6 +1,7 @@
 module.exports = {
   command: 'add',
   info: 'Adds user to group from entered number.',
+  private: false,
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in group!*' });
     if (!text && !msg.replied) return await msg.reply({ text: '*Please enter a number with +country code to add!*' });
