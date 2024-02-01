26e4c4f5822a03a453e5f4727ad1ebad3b2e339e
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 20:40:02 +0530

    Mode switcher

--- commands/unrestrict.js
+++ commands/unrestrict.js
@@ -1,6 +1,7 @@
 module.exports = {
   command: 'unrestrict',
   info: 'Everyone can edit group info mode.',
+  private: false,
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in group!*' });
     if (!(await msg.isAdmin(msg.me))) return await msg.reply({ text: '*I am not an admin of this group!*' });
