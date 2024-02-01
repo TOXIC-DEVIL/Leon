90d44c6e928ca41a3920ef5d5d828d82bb45d2d5
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 20:40:24 +0530

    Mode switcher

--- commands/url.js
+++ commands/url.js
@@ -3,6 +3,7 @@ const { telegraph } = require('../helpers/upload');
 module.exports = {
   command: 'url',
   info: 'Uploads image or video to telegraph.',
+  private: false,
   func: async (sock, msg) => {
     if (!msg.replied.image && !msg.replied.video) return await msg.reply({ text: '*Please reply to any image or video!*' });
     let media = await msg.load(msg.replied.image ? msg.replied.image : msg.replied.video);
