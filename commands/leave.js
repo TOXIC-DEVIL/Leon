c6563fc8587a9fbebcb4bfe8569223f47695ea5a
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 20:06:59 +0530

    Mode switcher

--- commands/leave.js
+++ commands/leave.js
@@ -1,6 +1,7 @@
 module.exports = {
   command: 'leave',
   info: 'Leaves from the group.',
+  private: true,
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in groups!*' });
     await msg.reply({ text: '*Leaving...*' }).then(async () => {
