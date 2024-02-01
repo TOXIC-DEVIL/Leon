4adb45bc4611383c93f7d60640727cadfc29dfd0
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 20:07:36 +0530

    Mode switcher

--- commands/link.js
+++ commands/link.js
@@ -1,6 +1,7 @@
 module.exports = {
   command: 'link',
   info: 'Fetches group\'s invitation link.',
+  private: false,
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in group!*' });
     if (!(await msg.isAdmin(msg.me))) return await msg.reply({ text: '*I am not an admin of this group!*' });
