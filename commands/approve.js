348efb3f5f2d5cfce0ba0caac9c9c49600df9c56
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 19:52:27 +0530

    Mode switcher

--- commands/approve.js
+++ commands/approve.js
@@ -1,6 +1,7 @@
 module.exports = {
   command: 'approve',
   info: 'Approves join request of given user.',
+  private: false,
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in group!*' });
     if (!(await msg.isAdmin(msg.me))) return await msg.reply({ text: '*I am not an admin of this group!*' });
