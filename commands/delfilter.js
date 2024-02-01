659308025ad8038e58e636311a2448ddeeca98da
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-01 19:59:33 +0530

    Mode switcher

--- commands/delfilter.js
+++ commands/delfilter.js
@@ -3,6 +3,7 @@ const { delFilter } = require('../helpers/database/filter');
 module.exports = {
   command: 'delfilter',
   info: 'deletes filter from the chat.',
+  private: true,
   func: async (sock, msg, text) => {
     if (!text) return await msg.reply({ text: '*Please enter the filter match to delete!*\n*Example:*\n*- ' + (process.env?.PREFIX || '/') + 'delfilter hey' });
     let filter = await delFilter(msg.chat, text);
