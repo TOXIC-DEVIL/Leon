3bb5d78bc983e75826943f1741f8bf2b1e1e75f3
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-04 14:30:55 +0530

    isAdmin?

--- commands/reject.js
+++ commands/reject.js
@@ -5,6 +5,7 @@ module.exports = {
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in group!*' });
     if (!(await msg.isAdmin(msg.me))) return await msg.reply({ text: '*I am not an admin of this group!*' });
+    if (!(await msg.isAdmin(msg.sender))) return await msg.reply({ text: '*You are not an admin of this group!*' });
     if (!text || !text.startsWith('+')) return await msg.reply({ text: '*Please enter the number of user with +countrycode for rejecting!*' });
     let list = [];
     let lis = await sock.groupRequestParticipantsList(msg.chat)
