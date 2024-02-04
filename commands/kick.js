35e61673dcab5f1a743220924520605a85414480
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-04 14:29:13 +0530

    isAdmin?

--- commands/kick.js
+++ commands/kick.js
@@ -6,6 +6,7 @@ module.exports = {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in groups!*' });
     if (!text && !msg.replied) return await msg.reply({ text: '*Please reply or mention any user!*' });
     if (!(await msg.isAdmin(msg.me))) return await msg.reply({ text: '*I am not an admin of this group!*' });
+    if (!(await msg.isAdmin(msg.sender))) return await msg.reply({ text: '*You are not an admin of this group!*' });
     if (msg.replied) {
       let isPart = await msg.isParticipant(msg.replied.sender, msg.chat);
       if (!isPart) return await msg.reply({ text: '*@' + msg.replied.sender.split('@')[0] + ' is already not a participant of this group.*' });
