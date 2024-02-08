bd87a133b4060b877b350e41ad5097d03c877b5d
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-02-08 18:16:52 +0530

    Fixed: 'poll' command.

--- commands/poll.js
+++ commands/poll.js
@@ -4,8 +4,8 @@ module.exports = {
   private: false,
   func: async (sock, msg, text) => {
     if (!text) return await msg.reply({ text: '*Please enter the title and options by splitting with | symbol!*\n\n*Example:*\n*- ' + (process.env?.PREFIX || '/') + 'poll title|option1|option2|option3*' });
-    if (!text.match(/|/)) return await msg.reply({ text: '*Invalid format, enter the title and options by splitting with | symbol!*\n\n*Example:*\n*- ' + (process.env?.PREFIX || '/') + 'poll title|option1|option2|option3*' });
-    if (text.match(/|/).length < 2) return await msg.reply({ text: '*There should be atleast 2 options for the poll.*' });
+    if (!/|/.test(text)) return await msg.reply({ text: '*Invalid format, enter the title and options by splitting with | symbol!*\n\n*Example:*\n*- ' + (process.env?.PREFIX || '/') + 'poll title|option1|option2|option3*' });
+    if (text.split('|').length < 2) return await msg.reply({ text: '*There should be atleast 2 options for the poll.*' });
     return await msg.reply({ poll: { title: text.split('|')[0], options: text.split('|').slice(1) } });
   }
 };
