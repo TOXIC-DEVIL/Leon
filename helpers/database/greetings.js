defe0084c1734903e279384e6b6e146e5c9990ef
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-03-11 16:54:05 +0530

    Fix: value too long for type character varying(255)

--- helpers/database/greetings.js
+++ helpers/database/greetings.js
@@ -12,7 +12,7 @@ const Greetings = database.define('Greetings', {
     allowNull: false,
   },
   text: {
-    type: DataTypes.STRING(4000),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
 });
