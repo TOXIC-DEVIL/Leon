018aaf0ef1ebc2d555cd160d4399d25b0ad26556
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-03-19 15:25:17 +0530

    Fix: dict command.

--- commands/dict.js
+++ commands/dict.js
@@ -7,7 +7,7 @@ module.exports = {
   func: async (sock, msg, text) => {
     if (!text) return await msg.reply({ text: '*Please enter any word!*' });
     try {
-      let data = await parseJson(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`);
+      let data = await parseJson(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`)[0];
       let word = data.word;
       let phonetics = data.phonetics[0].text;
       let partsOfSpeech = data.meanings[0].partOfSpeech;
@@ -15,7 +15,7 @@ module.exports = {
       let example = (data.meanings[0].definitions.find(obj => 'example' in obj) || {})['example'];
       return await msg.reply({ text: `_Word_ : *${word}*\n_Parts of speech_ : *${partsOfSpeech}*\n_Definition_ :\n*${definition}*${example == undefined ? `` : `\n_Example_ : *${example}*`}` });
     } catch {
-      return await msg.reply({ text: '*Unable to find defenition for ' + text + '!*' });
+      return await msg.reply({ text: '*Unable to find definition for ' + text + '!*' });
     }
   }
 };
