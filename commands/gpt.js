ff22575b457d6e9d0cece63a8253a1df3aa0d3ef
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-04-19 15:52:23 +0530

    Add: OpenAI ChatGPT

---
+++ commands/gpt.js
@@ -0,0 +1,11 @@
+const { ai } = require('../helpers/ai');
+
+module.exports = {
+  command: 'gpt',
+  info: 'ChatGPT is a generative AI developed by OpenAI.',
+  private: false,
+  func: async (sock, msg, text) => {
+    let result = await ai('gpt', text || 'Hi');
+    return await msg.reply({ text: result });
+  }
+};
