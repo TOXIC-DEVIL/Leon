267f9bd04ea83341fd59c7b599c8e3e3a190a48f
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-04-19 15:46:02 +0530

    Add: ai features

---
+++ helpers/ai.js
@@ -0,0 +1,23 @@
+const { RsnChat } = require('rsnchat');
+let { gpt, gemini, dalle } = new RsnChat('rsnai_hMCzXCerezIAEr91cdCfT2jt');
+
+function ai(type, prompt) {
+  try {
+    type = type.toLowerCase();
+    let result;
+    if (type == 'chatgpt') {
+      result = await gpt(prompt);
+    } else if (type == 'gemini') {
+      result = await gemini(prompt);
+    } else if (type == 'dalle') {
+      result = await dalle(prompt);
+    }
+    return result?.message || result?.url || false;
+  } catch {
+    return false;
+  }
+};
+
+module.exports = { 
+  ai
+};
