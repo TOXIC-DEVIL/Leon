9cb691a16240ce23ae32d055adbe7d57bf072a52
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-04-21 18:37:27 +0530

    Fix: OpenAI DALL-E

--- commands/imagine.js
+++ commands/imagine.js
@@ -1,4 +1,5 @@
-let { ai } = require('../helpers/ai');
+const { RsnChat } = require('rsnchat');
+let ai = new RsnChat('rsnai_1WZBGHRi6kA4cjyKNrZTuEVY');
 
 module.exports = {
   command: 'imagine',
@@ -6,7 +7,7 @@ module.exports = {
   private: false,
   func: async (sock, msg, text) => {
     if (!text) return await msg.reply({ text: '*Please enter any prompt to generate image!*\n\n*Example:*\n*- Dramatic tones, antique small toy pikachu on dirt road, vintage aesthetic*\n*- Dead Angel Bloody Wings Scary Face , high contast hdr, fog, HDR, super resolution, dazzling colors, cinematic light, dramatic lighting*' });
-    let image = await ai('dalle', text);
+    let image = ai.dalle(text).then((response) => response?.url);
     if (!image) return await msg.reply({ text: '*❌ Unable to generate image for given prompt!*\n_This may be because of invalid prompt or not-safe-for-work prompt._' });
     return await msg.reply({ image: { url: image } });
   }
