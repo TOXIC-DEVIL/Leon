bc7b0a7bd71b19dac52f9f8b37f2b34f0e2a404b
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-04-21 19:55:01 +0530

    Logging

--- commands/imagine.js
+++ commands/imagine.js
@@ -8,6 +8,7 @@ module.exports = {
   func: async (sock, msg, text) => {
     if (!text) return await msg.reply({ text: '*Please enter any prompt to generate image!*\n\n*Example:*\n*- Dramatic tones, antique small toy pikachu on dirt road, vintage aesthetic*\n*- Dead Angel Bloody Wings Scary Face , high contast hdr, fog, HDR, super resolution, dazzling colors, cinematic light, dramatic lighting*' });
     await ai.dalle(text).then(async (response) => {
+      console.log(response);
       return await msg.reply({ image: { url: response.url } });
     }).catch(async (e) => {
       return await msg.reply({ text: '*❌ Unable to generate image for given prompt!*\n_This may be because of invalid prompt or not-safe-for-work prompt._' });
