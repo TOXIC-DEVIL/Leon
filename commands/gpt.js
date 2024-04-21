772c23b69dd1a9f58893eb270ecf8fbb586767d7
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-04-21 18:56:43 +0530

    Fixed: ChatGPT

--- commands/gpt.js
+++ commands/gpt.js
@@ -6,8 +6,8 @@ module.exports = {
   info: 'ChatGPT is a generative AI developed by OpenAI.',
   private: false,
   func: async (sock, msg, text) => {
-    text = !text ? 'Hello' : text;
-    let result = ai.gpt(text).then((response) => response?.message?.trim());
-    return await msg.reply({ text: result });
+    return await ai.gpt(text || 'Hello').then(async (response) => {
+      await msg.reply({ text: response?.message?.trim() });
+    });
   }
 };
