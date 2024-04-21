7d4ae7a60a08d5ff8dba2ab3ddc53b4db1ff8ed8
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-04-21 18:34:19 +0530

    Fix: Gemini AI

--- commands/gemini.js
+++ commands/gemini.js
@@ -1,11 +1,13 @@
-const { ai } = require('../helpers/ai');
+const { RsnChat } = require('rsnchat');
+let ai = new RsnChat('rsnai_1WZBGHRi6kA4cjyKNrZTuEVY');
 
 module.exports = {
   command: 'gemini',
   info: 'Gemini is a multimodal AI (LLM) developed by Google.',
   private: false,
   func: async (sock, msg, text) => {
-    let result = await ai('gemini', text || 'Hello');
+    text = !text ? 'Hello' : text;
+    let result = ai.gemini(text).then((response) => response.message?.trim());
     return await msg.reply({ text: result });
   }
 };
