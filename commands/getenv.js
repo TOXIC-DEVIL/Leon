1b00b8621c1c53338c24cc57f809dab6a159b4ba
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-05-20 12:05:42 +0530

    platform configuration

--- commands/getenv.js
+++ commands/getenv.js
@@ -1,11 +1,12 @@
 const { Heroku } = require('../helpers/heroku');
+const { PLATFORM } = require('../config');
 
 module.exports = {
   command: 'getenv',
   info: 'Gets the value of environment variable of given key.',
   private: true,
   func: async (sock, msg, text) => {
-    if ((process.env?.PLATFORM).toLowerCase() !== 'heroku') return await msg.reply({ text: '*You must deploy bot on heroku for bot administrative commands.*' });
+    if (PLATFORM.toLowerCase() !== 'heroku') return await msg.reply({ text: '*You must deploy bot on heroku for bot administrative commands.*' });
     if (!text) return await msg.reply({ text: '*Please enter the environment variable key!*' });
     let value = await Heroku('getenv', text);
     if (!value) {
