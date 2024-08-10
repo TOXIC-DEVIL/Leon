d4218daca5b2e407b898cc22223568d386049d29
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-08-10 22:00:08 +0530

    tagall now supports multiple media

--- commands/tagall.js
+++ commands/tagall.js
@@ -7,15 +7,40 @@ module.exports = {
     if (msg.sender !== msg.me && (!(await msg.isAdmin(msg.sender)))) return await msg.reply({ text: '*You are not an admin!*' });
     let group = await sock.groupMetadata(msg.chat);
     let mentions = group.participants.map((user) => user.id);
-    if (!text && !msg.replied.text) {
-     return await msg.reply({
-       text: '```@' + mentions.join('\n@').replace(/@s.whatsapp.net/g, '') + '```',
-       mentions: mentions
-     });
-    } else {
+    if (text || msg.replied.text) {
      return await msg.reply({
        text: text || msg.replied.text,
        mentions
+     });
+    } else if (msg.replied && msg.replied.image) {
+     return await msg.reply({
+       video: await msg.load(msg.replied.image),
+       caption: msg.replied.image?.caption || '',
+       mentions
+     });
+    } else if (msg.replied && msg.replied.video) {
+     return await msg.reply({
+       video: await msg.load(msg.replied.video),
+       caption: msg.replied.video?.caption || '',
+       mentions
+     });
+    } else if (msg.replied && msg.replied.audio) {
+     return await msg.reply({
+       video: await msg.load(msg.replied.audio),
+       ptt: msg.replied.audio?.ptt || false,
+       mentions
+     });
+    } else if (msg.replied && msg.replied.document) {
+     return await msg.reply({
+       video: await msg.load(msg.replied.document),
+       caption: msg.replied.document?.caption || '',
+       mimetype: msg.replied.document.mimetype,
+       mentions
+     });
+    } else {
+     return await msg.reply({
+       text: '```@' + mentions.join('\n@').replace(/@s.whatsapp.net/g, '') + '```',
+       mentions: mentions
      });
     }
   }
