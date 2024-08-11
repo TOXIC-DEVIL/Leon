53cf317ddf86f47001d129e5adbba8f20b094840
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-08-11 08:36:05 +0530

    fix: tagall

--- commands/tagall.js
+++ commands/tagall.js
@@ -13,26 +13,30 @@ module.exports = {
        mentions
      });
     } else if (msg.replied && msg.replied.image) {
+     let img = await msg.load(msg.replied.image);
      return await msg.reply({
-       video: await msg.load(msg.replied.image),
+       image: img,
        caption: msg.replied.image?.caption || '',
        mentions
      });
     } else if (msg.replied && msg.replied.video) {
+     let vid = await msg.load(msg.replied.video);
      return await msg.reply({
-       video: await msg.load(msg.replied.video),
+       video: vid,
        caption: msg.replied.video?.caption || '',
        mentions
      });
     } else if (msg.replied && msg.replied.audio) {
+     let aud = await msg.load(msg.replied.audio);
      return await msg.reply({
-       video: await msg.load(msg.replied.audio),
+       audio: aud,
        ptt: msg.replied.audio?.ptt || false,
        mentions
      });
     } else if (msg.replied && msg.replied.document) {
+     let doc = await msg.load(msg.replied.document);
      return await msg.reply({
-       video: await msg.load(msg.replied.document),
+       document: doc,
        caption: msg.replied.document?.caption || '',
        mimetype: msg.replied.document.mimetype,
        mentions
