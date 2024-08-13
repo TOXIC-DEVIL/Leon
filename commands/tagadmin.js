755997cda604caf11a63514d1615c18824fde733
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-08-13 20:32:17 +0530

    add: tagadmin command

---
+++ commands/tagadmin.js
@@ -0,0 +1,14 @@
+module.exports = {
+  command: 'tagadmin',
+  info: 'Tags or mentions all admins of the group.',
+  private: false,
+  func: async (sock, msg, text) => {
+    if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in groups!*' });
+    let group = await sock.groupMetadata(msg.chat);
+    let mentions = group.participants.map((user) => user.admin == 'admin' || user.admin == 'superadmin' ?? user.id);
+    return await msg.reply({
+      text: '```@' + mentions.join('\n@').replace(/@s.whatsapp.net/g, '') + '```',
+      mentions: mentions
+    });
+  }
+}
