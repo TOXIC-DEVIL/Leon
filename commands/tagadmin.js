ee0943ce5134e90b3f34e20c7e8fc3c315d17e45
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-08-13 20:36:29 +0530

    fix: mentions[]

--- commands/tagadmin.js
+++ commands/tagadmin.js
@@ -5,7 +5,9 @@ module.exports = {
   func: async (sock, msg, text) => {
     if (!msg.isGroup) return await msg.reply({ text: '*This command can only be used in groups!*' });
     let group = await sock.groupMetadata(msg.chat);
-    let mentions = group.participants.map((user) => user.admin == 'admin' || user.admin == 'superadmin' ?? user.id);
+    let mentions = group.participants.map((user) => (
+      user.admin == 'admin' || user.admin == 'superadmin' ? user.id : null
+    )).filter(user => user != null);
     return await msg.reply({
       text: '```@' + mentions.join('\n@').replace(/@s.whatsapp.net/g, '') + '```',
       mentions: mentions
