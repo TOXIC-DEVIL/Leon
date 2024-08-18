8d53a433b11ed92ca3b3ffb52bdb60bf1259acb3
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-08-18 15:14:38 +0530

    feat: viewonce command.

---
+++ commands/viewonce.js
@@ -0,0 +1,20 @@
+module.exports = {
+  command: 'viewonce',
+  info: 'Converts view once media to standard media for unrestricted viewing and sharing.',
+  private: false,
+  func: async (sock, msg, text) => {
+    if (!msg.replied.viewonce) return await msg.reply({ text: '*Please reply to any viewonce message!*' });
+    if (msg.replied.image) {
+      let image = await msg.load(msg.replied.image);
+      return await msg.reply({ image: image, caption: msg.replied.image?.caption || '' });
+    } else if (msg.replied.video) {
+      let video = await msg.load(msg.replied.video);
+      return await msg.reply({ video: video, caption: msg.replied.video?.caption || '' });
+    } else if (msg.replied.audio) {
+      let audio = await msg.load(msg.replied.audio);
+      return await msg.reply({ audio: audio, ptt: msg.replied.audio?.ptt || false });
+    } else {
+      return await msg.reply({ text: '*❌ Unsupported media, reply to any viewonce image, video or audio!*' });
+    }
+  }
+};
