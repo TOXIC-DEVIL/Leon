79c7dd9f5b6050ed09ec18a54d126add4ac2d244
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-08-20 13:56:44 +0530

    Update viewonce.js

--- commands/viewonce.js
+++ commands/viewonce.js
@@ -6,13 +6,13 @@ module.exports = {
     if (!msg.replied.viewonce) return await msg.reply({ text: '*Please reply to any viewonce message!*' });
     if (msg.replied.image) {
       let image = await msg.load(msg.replied.image);
-      return await msg.reply({ image: image, caption: msg.replied.image?.caption || '' });
+      return await msg.reply({ image: image, mimetype: msg.replied.image.mimetype, caption: msg.replied.image?.caption || '' });
     } else if (msg.replied.video) {
       let video = await msg.load(msg.replied.video);
-      return await msg.reply({ video: video, caption: msg.replied.video?.caption || '' });
+      return await msg.reply({ video: video, mimetype: msg.replied.video.mimetype, caption: msg.replied.video?.caption || '' });
     } else if (msg.replied.audio) {
       let audio = await msg.load(msg.replied.audio);
-      return await msg.reply({ audio: audio, ptt: msg.replied.audio?.ptt || false });
+      return await msg.reply({ audio: audio, mimetype: msg.replied.audio.mimetype, ptt: msg.replied.audio?.ptt || false });
     } else {
       return await msg.reply({ text: '*❌ Unsupported media, reply to any viewonce image, video or audio!*' });
     }
