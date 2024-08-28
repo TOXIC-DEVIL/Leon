335b2a251787f7c3f282c5c519bb8f8ca4c85c0b
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-08-28 13:05:22 +0530

    hide online presence

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const { list, uninstall } = require('./helpers/database/commands');
 const { parseJson } = require('./helpers/utils');
 const { database } = require('./helpers/database.js');
-const { SESSION, ADMINS, MODE, PREFIX, PLATFORM, DEBUG } = require('./config');
+const { SESSION, ADMINS, MODE, PREFIX, ONLINE, PLATFORM, DEBUG } = require('./config');
 const Greetings = require('./helpers/database/greetings');
 const axios = require('axios');
 const pino = require('pino');
@@ -136,6 +136,7 @@ async function Connect() {
             if (!msg.message) return;
             msg = await require('./helpers/message')(msg, sock, store);
             if (msg.chat === 'status@broadcast') return;
+            if (ONLINE !== true) await sock.sendPresenceUpdate('unavailable', msg.chat);
 
             try {
              if (allCommands(msg.command) || msg.isPrivate) {
